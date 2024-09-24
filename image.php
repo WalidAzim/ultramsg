dcdbfc257f50cb3d915c98172b95761311b78cd3
Walid <100448107+WalidAzim@users.noreply.github.com>
2024-09-24 02:02:21 +0100

    Update image.php

--- image.php
+++ image.php
@@ -5,7 +5,7 @@ $token = "60r5480ejv67kjzg"; // Replace with your UltraMsg token
 
 // WhatsApp message details
 $to = "+212613306238"; // Replace with the recipient's phone number
-$image_path = "/images/img.jpg"; // Replace with your server's image path
+$image_path = "/img.jpg"; // Replace with your server's image path
 
 // Full URL of the image on your server
 $image_url = "https://localhost/rissala/test" . $image_path; // Ensure this points to the uploaded image on your server
